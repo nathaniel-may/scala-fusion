e408b43a2ac79b4667e78af7c77a17aea4e7b98f
Nathaniel May <nate.may@fauna.com>
2019-09-26 18:36:04 -0400

    trying existential types

--- src/main/scala/Fusion/Fused.scala
+++ src/main/scala/Fusion/Fused.scala
@@ -2,16 +2,17 @@ package Fusion
 
 import scala.languageFeature.higherKinds
 import scala.languageFeature.implicitConversions
+import scala.languageFeature.existentials
 import Stream.Empty
 
 object Fused {
 
   implicit class Fusable[A](list: Stream[A]) {
     def mapFused[B](f: A => B): Fuser[A, B] =
-      Fuser(Fused.mapFused[A, B](f), toCoLazyList(list))
+      new Fuser[A, B](Cons(Fused.mapFused[A, B](f), Nil[Op, A, A]), toCoLazyList(list))
 
     def filterFused(f: A => Boolean): Fuser[A, A] =
-      Fuser(Fused.filterFused[A](f), toCoLazyList(list))
+      new Fuser[A, A](Cons(Fused.filterFused[A](f), Nil[Op, A, A]), toCoLazyList(list))
   }
 
   implicit def unStream[A, B](fuser: Fuser[A, B]): Stream[B] = fuser.toStream
@@ -19,9 +20,9 @@ object Fused {
 
   def mapFused[A, B](f: A => B): Op[A, B] =
     cll =>
-      CoLazyList[B, Stream[A]](
+      CoLazyList[B](
         cll.state,
-        s => cll.next(s) match {
+        (s: S forSome { type S }) => cll.next(s) match {
           case Done()          => Done()
           case Skip(sNext)     => Skip(sNext)
           case Yield(a, sNext) => Yield(f(a), sNext)
@@ -30,7 +31,7 @@ object Fused {
 
   def filterFused[A](f: A => Boolean): Op[A, A] =
     cll =>
-      CoLazyList[A, Stream[A]](
+      CoLazyList[A](
         cll.state,
         s => cll.next(s) match {
           case done@Done()              => done
@@ -45,41 +46,37 @@ object Fused {
   private[Fusion] case class Skip[A, S](s: S) extends Step[A, S]
   private[Fusion] case class Yield[A, S](a: A, s: S) extends Step[A, S]
 
-  private[Fusion] case class CoLazyList[A, S](state: S, next: S => Step[A, S])
+  private[Fusion] case class CoLazyList[A](all: (S, S => Step[A, S]) forSome { type S }) {
+    val state = all._1
+    val next = all._2
+  }
 
-  private[Fusion] def toCoLazyList[A](list: Stream[A]): CoLazyList[A, Stream[A]] =
+  private[Fusion] def toCoLazyList[A](list: Stream[A]): CoLazyList[A] =
     CoLazyList(
-      list,
-      {
-        case Empty    => Done()
-        case x #:: xs => Yield(x, xs)
-      }
+      (list,
+        (in: Stream[A]) => {
+          case Empty    => Done()
+          case x #:: xs => Yield(x, xs)
+        })
     )
 
-  private[Fusion] def toLazyList[A, S](co: CoLazyList[A, S]): Stream[A] = {
-    def go(s: S, next: S => Step[A, S]): Stream[A] = next(s) match {
+  private[Fusion] def toLazyList[A](co: CoLazyList[A]): Stream[A] = {
+    def go(all: (S, S => Step[A, S]) forSome { type S }): Stream[A] = all._2(all._1) match {
       case Done()          => Stream.empty
-      case Skip(sNext)     => go(sNext, next)
-      case Yield(a, sNext) => a #:: go(sNext, next)
+      case Skip(sNext)     => go(sNext, all._2)
+      case Yield(a, sNext) => a #:: go(sNext, all._2)
     }
 
     go(co.state, co.next)
   }
 
-  type CLL[A, B] = CoLazyList[B, Stream[A]]
-  type Op[A, B] = CLL[A, A] => CLL[A, B]
+  type Op[A, B] = CoLazyList[A] => CoLazyList[B]
   type Ops[A, B] = Thrist[Op, A, B]
 
-  // emulates GHC rewrite rules which are unavailable in Scala
-  private[Fusion] case class Fuser[A, B] (ops: Ops[A, B], state: CLL[A, A]) {
-    implicit val composableOp = new Monoid[Fused.Op] {
-      override def id[C]: Op[C, C] = identity
-      override def compose[C, D, E](a: Op[C, D], b: Op[D, E]): Op[C, E] =
-        b.compose[CLL[C, D]](a)
-    }
-
+  // emulates GHC rewrite rules which are unavailable in Scalac
+  private[Fusion] case class Fuser[A, B] (ops: Ops[A, B], state: CoLazyList[A]) {
     private[Fusion] def toStream: Stream[B] =
-      Thrist.compose[Op, A, B]((a, b) => CoLazyList.apply(b, a), state, ops)(state)
+      Thrist.compose[Op, A, B](_ => state, (a: Op[_, A], b: Op[A, _]) => b compose a, ops)(state)
 
     private[Fusion] def prepend[C](op: Op[B, C]): Fuser[A, C] =
       new Fuser[A, C](Cons[Op, A, B, C](op, ops), state)
@@ -93,16 +90,6 @@ object Fused {
       Fuser(Cons[Op, A, B, B](Fused.filterFused(f), ops), state)
   }
 
-  private[Fusion] object Fuser {
-    def apply[A, B](op: Op[A, B], s: CLL[A, A]) = new Fuser[A, B](Cons(op, Nil[CLL, A, A]), s)
-  }
-
-}
-
-// type class TODO scalaz? cats?
-trait Monoid[M[_, _]] {
-  def id[A]: M[A, A]
-  def compose[A, B, C](a: M[A, B], b: M[B, C]): M[A, C]
 }
 
 //Thrist of functions
@@ -127,8 +114,8 @@ case class Cons[Arr[_, _], A, B, C](head: Arr[B, C], tail: Thrist[Arr, A, B]) ex
 //case class Cons[Arr[_[_],_[_]], A[_], B[_], C[_]](head: Arr[A, B], tail: Thrist[Arr, B, C]) extends Thrist[Arr, A, C]
 
 object Thrist {
-  def compose[Arr[_, _], A, B, C](thrist: Thrist[Arr, A, B])(implicit ev: Monoid[Arr]): Arr[A, B] = thrist match {
-    case Nil(_)           => default
-    case Cons(head, tail) => ev.compose(head, compose(tail))
+  def compose[Arr[_, _], A, B](z: Arr[A, B], build:((Arr[C, D], Arr[D,E]) => Arr[C, E]) forSome { type C; type D; type E}, thrist: Thrist[Arr, A, B]): Arr[A, B] = thrist match {
+    case Nil(_)           => z
+    case Cons(head, tail) => build(head, compose(z, build, tail))
   }
 }
